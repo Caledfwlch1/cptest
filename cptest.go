120d63c6b292b9167ed551551e3af8e5fb6eea5e
andy <caledfwlch1@gmail.com>
2018-04-24 14:51:20 +0300

    finished

--- cptest.go
+++ cptest.go
@@ -4,19 +4,30 @@ import (
 	"flag"
 	"log"
 	"os"
-	"fmt"
 	"math/rand"
 	"time"
+	"path"
+	"strconv"
+	"io/ioutil"
+	"os/signal"
 )
 
-const blockGenData = 1024*1024
+const (
+	blockGenData = 1024*1024
+)
 
-var path *string
-var size *int
+var (
+	dir *string
+	size *int
+	delta *int
+	maxRepeat *int
+)
 
 func init(){
-	path = flag.String("path", "", "destination path")
-	size = flag.Int("size", 1024, "file size")
+	dir = flag.String("dir", "", "destination dir")
+	size = flag.Int("size", 512, "file size")
+	delta = flag.Int("delta", 100, "maximum number of files")
+	maxRepeat = flag.Int("max", 1024, "maximum number of files")
 
 	rand.Seed(time.Now().Unix())
 }
@@ -24,56 +35,110 @@ func init(){
 func main(){
 	flag.Parse()
 
-	if !chechParam(path, size) {
+	if !checkParam(dir, size, delta, maxRepeat) {
 		return
 	}
 
-	fmt.Printf("path %s, size %d\n", path, size)
+	log.Printf("destination directory: %s, file size: %d, number of files: %d\n", *dir, *size, *maxRepeat)
 
-	var (
-		data chan []byte
-		stop chan struct{}
-	)
+	data := make(chan []byte)
+	stop := make(chan struct{})
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
 
 	go genData(data, stop, *size)
 
-	for i := 0; i < 10; i++ {
-		d := <-data
-		fmt.Printf("%s\n", d)
+	fullTime := time.Now()
+	curTime := time.Now()
+	j := 0
+	i := 0
+	STOP:
+	for i = 0; i < *maxRepeat; i++ {
+		select {
+		case <-c:
+			close(stop)
+			break STOP
+		default:
+		}
+
+		if j == *delta {
+			curr := time.Now().Sub(curTime)
+			log.Printf("processing time %d of files: %s \t file per seconds: %f\n", *delta, curr.String(), float64(j) / curr.Seconds())
+			curTime = time.Now()
+			j = 0
+		}
+		j++
+		name := randomName(*dir)
+		time.Sleep(time.Millisecond )
+		if err := writeToFile(name, <-data); err != nil {
+			log.Fatalf("error writing to file %s %+v", name, err)
+		}
+
 	}
+	log.Println("------------------------------------------------------------------------------")
+	total := time.Now().Sub(fullTime)
+	log.Printf("total processing time %d files: %s \t file per seconds: %f\n", i, total.String(), float64(i) / total.Seconds())
 
 }
 
-func chechParam(s *string, i *int) bool {
+func checkParam(s *string, i, delta, max *int) bool {
 	if *s == "" {
-		log.Printf("destination path is empty")
+		log.Printf("destination dir is empty")
 		return false
 	}
-	if st, err := os.Stat(*s); os.IsNotExist(err) || !st.IsDir() {
-		log.Printf("destination is not exist or not a dirrectory")
+	st, err := os.Stat(*s)
+	if err == nil && !st.IsDir() {
+		log.Printf("destination is not a dirrectory")
 		return false
 	}
+	if err == nil {
+		log.Printf("dirrectory %s is exist", *s)
+		log.Println("removing dirrectory...")
+		if err = os.RemoveAll(*s); err != nil {
+			log.Printf("error removing directory %s %+v", *s, err)
+			return false
+		}
+		log.Println("removing dirrectory... done")
+		err = os.ErrNotExist
+	}
+
+	if os.IsNotExist(err) {
+		if err = os.MkdirAll(*s, 0777); err != nil {
+			return false
+		}
+	}
+
 	if *i <=0 {
 		log.Printf("invalid size")
+		return false
+	}
+
+	if *delta <=0 {
+		log.Printf("invalid number of delta")
+		return false
+	}
+	if *max <=0 {
+		log.Printf("invalid number of files")
 		return false
 	}
 	return true
 }
 
-func genData(data chan<- []byte, stop <-chan struct{}, size int){
-	randSlice := make([]byte, size)
-	blockData := make([]byte, blockGenData)
-
+func genData(data chan []byte, stop <-chan struct{}, size int){
 	for {
+		randSlice := make([]byte, size)
 		if size <= blockGenData {
 			makeRandomSlice(randSlice)
 		} else {
-			for i := 0; i < len(randSlice)%blockGenData; {
+			blockData := make([]byte, blockGenData)
+			i := 0
+			for i < len(randSlice){
 				makeRandomSlice(blockData)
 				copy(randSlice[i:], blockData)
-				i += size
+				i += blockGenData
 			}
 		}
+
 		select {
 		case <-stop:
 			return
@@ -85,4 +150,12 @@ func genData(data chan<- []byte, stop <-chan struct{}, size int){
 
 func makeRandomSlice(p []byte) {
 	rand.Read(p)
+}
+
+func randomName(fulldir string) string {
+	return path.Join(fulldir, "test-" + strconv.FormatInt(rand.Int63(), 16))
+}
+
+func writeToFile(name string, d []byte) error {
+	return ioutil.WriteFile(name, d, 0666)
 }
